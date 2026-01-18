b8a735a4eec5d8b1ad9e0f963bcdd8310e83009c
mrbannerbear <sayedsaqlayn@gmail.com>
2026-01-18 14:09:30 +0600

    added supabase util functions for client and server side

---
+++ lib/supabase/server.ts
@@ -0,0 +1,35 @@
+import { createServerClient } from "@supabase/ssr";
+import { cookies } from "next/headers";
+
+const getEnvVariables = () => {
+  const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+  const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY;
+
+  if (!supabaseUrl || !supabaseKey) {
+    throw new Error(
+      "Missing NEXT_PUBLIC_SUPABASE_URL or NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY environment variables",
+    );
+  }
+
+  return { supabaseUrl, supabaseKey };
+};
+
+export async function createClient() {
+  const cookieStore = await cookies();
+  const { supabaseUrl, supabaseKey } = getEnvVariables();
+
+  return createServerClient(supabaseUrl, supabaseKey, {
+    cookies: {
+      getAll() {
+        return cookieStore.getAll();
+      },
+      setAll(cookiesToSet) {
+        try {
+          cookiesToSet.forEach(({ name, value, options }) =>
+            cookieStore.set(name, value, options),
+          );
+        } catch {}
+      },
+    },
+  });
+}
